d8053e0e46cd8a2054e2a0877f8770ca5cd3df77
Nemanja Savatic <savatic.nemanja@gmail.com>
2026-06-20 23:05:22 +0200

    Add Google Tag Manager and Meta Pixel tracking
    Import analytics from main branch:
    - Google Tag Manager (GTM-WFX2G7LD) with async script loading
    - Meta Pixel / Facebook Conversion API (975827895284079)
    - Noscript fallbacks for both GTM and Meta Pixel
    - Favicon and manifest links in metadata
    - Geographic meta tags (geo.region, ICBM coordinates)
    - Apple web app configuration
    
    Scripts load with 'afterInteractive' strategy for optimal performance.
    Enables conversion tracking and audience insights for marketing campaigns.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { Manrope, Noto_Serif } from 'next/font/google';
 import './globals.css';
 import { ToastProvider } from '@/components/ToastProvider/ToastProvider';
@@ -27,16 +28,83 @@ export const metadata = {
     default: 'Avala Home Concept',
     template: '%s | Avala Home Concept',
   },
+  manifest: '/site.webmanifest',
+  icons: [
+    { rel: 'icon', type: 'image/png', sizes: '96x96', url: '/favicon-96x96.png' },
+    { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+    { rel: 'shortcut icon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/apple-touch-icon.png' },
+  ],
+  appleWebApp: {
+    title: 'Avala Home Concept',
+  },
 };
 
 export const viewport = {
   themeColor: '#C4975A',
+  width: 'device-width',
+  initialScale: 1.0,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang={defaultLocale} className={`${notoSerif.variable} ${manrope.variable}`}>
+      <head>
+        {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '975827895284079');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WFX2G7LD');
+            `,
+          }}
+        />
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WFX2G7LD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=975827895284079&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <ToastProvider>
           <SmoothScroll />
           {children}
